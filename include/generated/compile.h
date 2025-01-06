43fa4ff3c43c6629d60a186606be252cea8fce21
lzz <986046679@qq.com>
2025-01-05 19:48:58 -0800

    change commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#2 SMP PREEMPT Thu Dec 26 18:52:48 PST 2024"
+#define LINUX_COMPILE_BY "tina"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 5.3.1 20160412 (Linaro GCC 5.3-2016.05) "
